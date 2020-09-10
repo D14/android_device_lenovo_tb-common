35f54ba7e53d037d085c466705fd3f4f2e11f283
ToxaD14 <toxad14@gmail.com>
2020-09-10 20:41:35 +0300

    TB8703F: fix reboot to recovery on system startup

--- prebuilt/bin/deunify.sh
+++ prebuilt/bin/deunify.sh
@@ -15,14 +15,16 @@ rm -rf /system/system/vendor/firmware/variant
 
 # Remove telephony files for wifi variant
 if [ "$baseband" == "apq" ]; then 
-    rm -rf /system/system/app/datastatusnotification
-    rm -rf /system/system/app/messaging
-    rm -rf /system/system/app/QtiTelephonyService
-    rm -rf /system/system/app/SecureElement
-    rm -rf /system/system/app/SimAppDialog
-    rm -rf /system/system/app/Stk
-    rm -rf /system/system/priv-app/CarrierConfig
-    rm -rf /system/system/priv-app/CellBroadcastReceiver
-    rm -rf /system/system/priv-app/Dialer
-    rm -rf /system/system/priv-app/TeleService
+    rm -rf /system/app/datastatusnotification
+    rm -rf /system/app/messaging
+    rm -rf /system/app/QtiTelephonyService
+    rm -rf /system/app/SecureElement
+    rm -rf /system/app/SimAppDialog
+    rm -rf /system/app/Stk
+    rm -rf /system/priv-app/CarrierConfig
+    rm -rf /system/priv-app/CellBroadcastReceiver
+    rm -rf /system/priv-app/Dialer
+    rm -rf /system/priv-app/Telecom
+    rm -rf /system/priv-app/TelephonyProvider
+    rm -rf /system/priv-app/TeleService
 fi
